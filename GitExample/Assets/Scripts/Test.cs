f8c74f07d8d6a85dab24f514a7bcdae1c4dadc41
Valentijn Muijrers <vmuijrers@gmail.com>
2022-02-09 14:17:59 +0100

    changed script

--- GitExample/Assets/Scripts/Test.cs
+++ GitExample/Assets/Scripts/Test.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     public GameObject someObject;
+    public Transform test;
     // Start is called before the first frame update
     void Start()
     {
